50db3a7ef89228194f089340bba6f947b3b5ddcc
tptadpole <wk7293@gmail.com>
2021-04-07 11:07:27 +0800

    處理welcome.blade.php - 暫定樣板<div class="row row-cols-1 row-cols-md-4">

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,25 +12,23 @@
         @else
             <p>遊客</p>
         @endcan
-    </div>
-    <div class="container">
-        <div class="card">
-            <ul class="list-group">
-                @foreach( $homepageProducts as $homepageProduct )
-                    <li class="list-group-item">
-                        <ul type="disc"><li>{{$homepageProduct['name']}}</li> </ul>     
-                    </li>
-                    <li class="list-group-item">
-                        <ul type="disc"><li>{{$homepageProduct['description']}}</li> </ul>     
-                    </li>
-                    <li class="list-group-item">
-                        <ul type="disc"><li>{{$homepageProduct['vote']}}</li> </ul>     
-                    </li>
-                    <li class="list-group-item">
-                        <ul type="disc"><li>{{$homepageProduct['comment']}}</li> </ul>     
-                    </li>
-                @endforeach
-            </ul>
+
+        <div class="row row-cols-1 row-cols-md-4">
+            @foreach( $homepageProducts as $homepageProduct ) 
+            <div class="col mb-4">
+              <div class="card h-100">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6c757d"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+          
+                <div class="card-body">
+                  <h5 class="card-title">{{$homepageProduct['name']}}</h5>
+                  <p class="card-text">{{$homepageProduct['description']}}</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">{{$homepageProduct['comment']}}</small>
+                </div>
+              </div>
+            </div>
+            @endforeach
         </div>
     </div>
 @endsection
